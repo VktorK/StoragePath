bf04e16ef86acdfd54dd7f4e50dd812179b9c972
v.korochanskiy <v.korochanskiy@nextcontact.ru>
2025-05-02 10:26:02 +0400

    сделал навигацию кнопки Домой

--- resources/views/storage/index.blade.php
+++ resources/views/storage/index.blade.php
@@ -34,8 +34,11 @@
     </table>
 
 
-    @if ($path)
+    @if ($path !== '.' && !empty(trim($path, '/')))
         <a href="{{ route('storage.index', ['path' => dirname($path)]) }}" class="btn btn-secondary">Назад</a>
+    @endif
+    @if ($path == '.' && !empty(trim($path, '/')))
+        <a href="{{ route('welcome') }}" class="btn btn-secondary">Домой</a>
     @endif
 </div>
 
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\StorageController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/storage', [StorageController::class, 'index'])->name('storage.index');
