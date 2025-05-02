5db10d19c994aad5a002fb86f83c208cc9c8e3a0
v.korochanskiy <v.korochanskiy@nextcontact.ru>
2025-05-02 12:54:16 +0400

    сделал навигацию кнопки Домой

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -117,7 +117,7 @@
                             </span>
                             <span>
                                 Look the
-                                <a href="{{ route('storage.index') }}" target="_blank" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
+                                <a href="{{ route('storage.index') }}" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
                                     <span>Storage</span>
                                     <svg
                                         width="10"
